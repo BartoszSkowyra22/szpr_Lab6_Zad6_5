05973b53f014b696eeef25d3f507cd984f9b2fc1
Bartosz S <skowyrab@gmail.com>
2024-04-24 21:56:38 +0200

    szpr_Lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import Result from './components/Result'
+import Keypad from "./components/Keypad"
+import './App.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [state, setState] = useState({ result: "" })
+  const onClick = button => {
+    switch (button) { case "=":
+      calculate()
+      break
+      case "C":
+      reset()
+      break
+      case "CE":
+      backspace()
+      break
+      default:
+      setState({ result: state.result + button }) }
+  }
 
-export default App;
+  const calculate = () => {
+    try {
+      let result;
+      if (eval(state.result) === Infinity || eval(state.result) === -Infinity) {
+        result = "Nie dziel przez 0!";
+      } else {
+        result = eval(state.result) + "";
+      }
+      setState({
+        result: result
+      });
+    } catch (e) {
+      setState({
+        result: "error"
+      })
+    }
+  }
+
+  const reset = () => { setState({
+    result: "" })
+  }
+  const backspace = () => {
+    setState({
+      result: state.result.slice(0, -1)
+    }) }
+  return ( <div>
+        <div className="srodek"> <h3>Kalkulator</h3>
+          <Result result={state.result} /> <Keypad onClick={onClick} />
+        </div> </div>
+  ) }
+export default App
